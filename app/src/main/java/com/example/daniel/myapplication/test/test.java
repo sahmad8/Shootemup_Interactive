4539483e6b07dabc3e456a8bb9924076ab44f4de
Daniel <dannz512@gmail.com>
2015-04-14 12:46:03 -0500

    addition of testing

---
+++ app/src/main/java/com/example/daniel/myapplication/test/test.java
@@ -0,0 +1,14 @@
+package com.example.daniel.myapplication.test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by Daniel on 4/14/2015.
+ */
+public class test extends InstrumentationTestCase {
+    public void test() throws Exception {
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
+}
